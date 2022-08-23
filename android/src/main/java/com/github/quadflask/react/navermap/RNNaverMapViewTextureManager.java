095fea398df90fb46094a39e94ee20c39c5cf8f5
juntaekHong <50249009+juntaekHong@users.noreply.github.com>
2022-08-23 15:33:16 +0900

    네이버 지도 Init 위경도,줌 레벨 설정 추가

--- android/src/main/java/com/github/quadflask/react/navermap/RNNaverMapViewTextureManager.java
+++ android/src/main/java/com/github/quadflask/react/navermap/RNNaverMapViewTextureManager.java
@@ -3,6 +3,8 @@ package com.github.quadflask.react.navermap;
 import androidx.annotation.NonNull;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.naver.maps.map.NaverMapOptions;
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.CameraPosition;
 
 public class RNNaverMapViewTextureManager extends RNNaverMapViewManager {
     public RNNaverMapViewTextureManager(ReactApplicationContext context) {
@@ -19,6 +21,7 @@ public class RNNaverMapViewTextureManager extends RNNaverMapViewManager {
     protected NaverMapOptions getNaverMapViewOptions() {
         return new NaverMapOptions()
                 .useTextureView(true)
-                .translucentTextureSurface(true);
+                .translucentTextureSurface(true)
+                .camera(new CameraPosition(new LatLng(35.1798159, 129.0750222), 14));
     }
 }
